f8002ec645dfeb4e3220f7856c4f6bfb5458b555
Jonas <xgv866@alumni.ku.dk>
2024-07-03 16:37:10 +0200

    update images and project order

--- src/javascript/loader.js
+++ src/javascript/loader.js
@@ -1,6 +1,16 @@
 // loader.js
 window.addEventListener('load', function() {
+    const loadingScreen = document.querySelector('.loading-screen');
+    const loadingText = document.querySelector('.loading-screen .loading');
+
     setTimeout(function() {
-        document.body.classList.add('loaded');
-    }, 2500);
+        // Fade out the loading text
+        loadingText.style.opacity = '0';
+        loadingText.style.filter = 'blur(10px)';
+
+        // After the text fades out, fade out the entire loading screen
+        setTimeout(function() {
+            document.body.classList.add('loaded');
+        }, 550); // This should match the transition duration in CSS
+    }, 2750);
 });
